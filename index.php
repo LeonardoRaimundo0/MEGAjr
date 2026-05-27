54174c9010dbdb9b0aa64bd447c1b95154f51138
Leonardo <leorraimundo@gmail.com>
2026-05-27 19:52:18 +0100

    Titulo 1 Abu

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $featuredAds = dbt_seed_market_ads();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DBT - Inicio</title>
+    <title>DBT - Inicio 1 Abu</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
