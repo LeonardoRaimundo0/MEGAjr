4c4c0ecba6da1a60245eab2a4df30186696ae3ba
Leonardo <leorraimundo@gmail.com>
2026-05-27 19:56:04 +0100

    titulo 2 AAAAAbu

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $featuredAds = dbt_seed_market_ads();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DBT - Inicio</title>
+    <title>DBT - Inicio 2 AAAAbu</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
